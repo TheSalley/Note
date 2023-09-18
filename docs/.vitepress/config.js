efa2f878dde4228e1c1aa94cd9a4debaa48e599c
theshire <1919532973@qq.com>
2023-09-18 22:35:51 +0800

    修复图片防盗链

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,7 +9,14 @@ module.exports = {
       "link",
       {
         rel: "icon",
-        href: "https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/641ad29515da4786a74d943a931ef25c~tplv-k3u1fbpfcp-watermark.image?",
+        href: "https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/641ad29515da4786a74d943a931ef25c~tplv-k3u1fbpfcp-watermark.image",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "referrer",
+        content: "no-referrer",
       },
     ],
   ],
