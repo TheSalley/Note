880fadd375b334d85cf2869b7c09f62184c1a939
wangmingqiang <wangmq53396@hundsun.com>
2024-05-07 16:35:39 +0800

    修改目录设计

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,22 +3,7 @@ module.exports = {
   lang: "en-US",
   description: "学习笔记",
   outDir: "../build",
-  head: [
-    [
-      "link",
-      {
-        rel: "icon",
-        href: "https://p3-juejin.byteimg.com/tos-cn-i-k3u1fbpfcp/641ad29515da4786a74d943a931ef25c~tplv-k3u1fbpfcp-watermark.image",
-      },
-    ],
-    [
-      "meta",
-      {
-        name: "referrer",
-        content: "no-referrer",
-      },
-    ],
-  ],
+  head,
   themeConfig: {
     nav: [
       {
